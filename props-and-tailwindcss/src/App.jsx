db84785739fd681a2c511bdef97601f0dc1a2ad8
Niramay33 <thakerniramay@gmail.com>
2025-01-27 18:43:25 +0530

    Learning props and default props handling

--- props-and-tailwindcss/src/App.jsx
+++ props-and-tailwindcss/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/card'
 
 function App() {
-
+  let a = 'Hello';
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="bg-green-400 text-black rounded-xl p-4">
         Hello world!
       </h1>
+
+      <Card username={a} />
+
     </>
   )
 }
