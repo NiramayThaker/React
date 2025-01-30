e8993c4896425352fec10dab9742156ca0544244
Niramay33 <thakerniramay@gmail.com>
2025-01-30 20:46:36 +0530

    Binding components route links

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '.componenets/Header/Header'
-import Footer from '.componenets/Footer/Footer'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
