3648f9ca81418a9be7a7d5a4647de1dba3e98899
Niramay33 <thakerniramay@gmail.com>
2025-02-02 08:09:59 +0530

    Boilerplate set up and app design

--- theamChanger/src/App.jsx
+++ theamChanger/src/App.jsx
@@ -7,11 +7,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+
+    <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                {/*  TheamBtn */}
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+                {/*  Card */}
+            </div>
+        </div>
+    </div>
+
   )
 }
 
