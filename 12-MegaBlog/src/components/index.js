4df42727bd8ea1904b3ab55f50f0ffd7649487f1
Niramay33 <thakerniramay@gmail.com>
2025-02-18 23:18:05 +0530

    Making protected routes and components

--- 12-MegaBlog/src/components/index.js
+++ 12-MegaBlog/src/components/index.js
@@ -1,7 +1,31 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import { Logo } from './Logo';
-import { LogoutBtn } from './Header/LogoutBtn';
-import { Container } from "./container/Container";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import {Input} from "./input";
+import AuthLayout from "./AuthLayout";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
